ac76bec7f01b0e682f63b7256db532f33a97f2b8
MatthewKociolekk <mkociolek@cps.edu>
2023-12-04 09:52:08 -0600

    finishes par 1

--- part1-linear-regression/example1_checking_correlation.py
+++ part1-linear-regression/example1_checking_correlation.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # gets the data and sets x and y values
-data = pd.read_csv("chirping_data.csv")
+data = pd.read_csv("part1-linear-regression/chirping_data.csv")
 x = data["Temp"]
 y = data["Chirps"]
 
@@ -12,12 +12,12 @@ plt.figure(figsize=(6,4))
 
 # creates a scatter plot and labels the axes
 plt.scatter(x,y)
-plt.xlabel("Temperature °F")
-plt.ylabel("Chirps per Minute")
-plt.title("Cricket Chirps by Temperature")
+plt.xlabel("blood pressure")
+plt.ylabel("age")
+plt.title("Blood pressure by age")
 
 # prints the correlation coefficient
-print(f"Correlation between Temperature and Chirps/Min: {x.corr(y)}")
+print(f"Correlation between age and blood pressure: {x.corr(y)}")
 
 # show the plot
 plt.show()
